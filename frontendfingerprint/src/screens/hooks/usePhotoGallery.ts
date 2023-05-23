a298600ede042c0ea11a0500e014033900d4a62e
Navani-cpu <doolocompany@gmail.com>
2023-05-23 15:53:43 +0530

    fix(camera-web-fix): camera is showing in web view

--- frontendfingerprint/src/screens/hooks/usePhotoGallery.ts
+++ frontendfingerprint/src/screens/hooks/usePhotoGallery.ts
@@ -7,15 +7,32 @@ import {Preferences} from "@capacitor/preferences"
 import {Capacitor} from "@capacitor/core"
 
 export function usePhotoGallery() {
+	const [photos, setPhotos] = useState<UserPhoto[]>([])
+
 	const takePhoto = async () => {
-		const photo = await Camera.getPhoto({
-			resultType: CameraResultType.Uri,
-			source: CameraSource.Camera,
-			quality: 100,
-		})
+		try {
+			const photo = await Camera.getPhoto({
+				resultType: CameraResultType.Uri,
+				source: CameraSource.Camera,
+				quality: 100,
+			})
+			console.log("photo :::", photo)
+			const fileName = new Date().getTime() + ".jpeg"
+			const newPhotos = [
+				{
+					filepath: fileName,
+					webviewPath: photo.webPath,
+				},
+				...photos,
+			]
+			setPhotos(newPhotos)
+		} catch (error) {
+			console.error("Error taking photo", error)
+		}
 	}
 
 	return {
 		takePhoto,
+		photos,
 	}
 }
